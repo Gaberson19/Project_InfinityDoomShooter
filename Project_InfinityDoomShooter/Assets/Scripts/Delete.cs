ece9cfbf912717a8ff8f1839d4aa64f3ec6c8c89
Piolo-Ambion <peambion@student.apc.edu.ph>
2022-11-13 04:51:26 +0800

    Delete Projectile
    Projectile deletes after a certain amount of time

---
+++ Project_InfinityDoomShooter/Assets/Scripts/Delete.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Delete : MonoBehaviour
+{
+    private void Awake()
+    {
+        StartCoroutine(waiter());
+    }
+
+    IEnumerator waiter()
+    {
+        yield return new WaitForSeconds(3);
+        Object.Destroy(this.gameObject);
+    }
+}
