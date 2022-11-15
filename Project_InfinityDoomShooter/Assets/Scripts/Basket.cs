18e4617a0a42a7acbbd94deaaaabe71f4f510d79
rpcagoco <69941216+rpcagoco@users.noreply.github.com>
2022-11-15 14:04:46 +0800

    Final

---
+++ Project_InfinityDoomShooter/Assets/Scripts/Basket.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Basket : MonoBehaviour
+{
+    int speedAmt = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.x <= -6)
+        {
+            speedAmt = 10;
+        }
+        if(transform.position.x >= 6)
+        {
+            speedAmt = -10;
+        }
+        transform.Translate(speedAmt*Time.deltaTime, 0, 0);
+    }
+}
